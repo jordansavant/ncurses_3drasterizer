91630edf6052a700ed796ae37f9074d22a72a6d9
Jordan Savant <md5madman@gmail.com>
2020-09-12 21:56:26 +0000

    remove scanline log

--- main_rasterize_triangle.c
+++ main_rasterize_triangle.c
@@ -95,7 +95,6 @@ void plot(WINDOW* win, int x, int y, int colorpair) {
 }
 
 void draw_scanline(WINDOW* win, int y, int x1, int x2, int cp) {
-	xlogf("dsline y=%d x1=%d x2=%d", y, x1, x2);
 	for (int x=x1; x < x2; x++) {
 		plot(win, x, y, cp);
 	}
